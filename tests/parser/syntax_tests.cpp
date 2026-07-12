0744012a8218c6c0d074b8bd65663bb2272a875f
lmingzhi618 <lmingzhi618@gmail.com>
2026-07-12 16:13:40 -0400

    test(parser): split syntax and parser behavior tests Refs #42

--- tests/parser/syntax_tests.cpp
+++ tests/parser/syntax_tests.cpp
@@ -1,4 +1,3 @@
-#include "parser/parser.hpp"
 #include "parser/syntax.hpp"
 
 #include <cassert>
@@ -43,30 +42,5 @@ int main() {
         assert(file.declarations[0].fields[0].location.line == 4);
     }
 
-    {
-        auto ok = cjm::parser::parse_source_file("examples/basic/user.hpp");
-        assert(ok.success);
-        assert(ok.file.path == "examples/basic/user.hpp");
-        assert(ok.file.declarations.size() == 1);
-        assert(ok.file.declarations[0].name == "User");
-        assert(ok.file.declarations[0].fields.size() == 2);
-        assert(ok.file.declarations[0].fields[0].name == "name");
-        assert(ok.file.declarations[0].fields[0].type_spelling ==
-               "std::string");
-        assert(ok.file.declarations[0].fields[1].name == "age");
-        assert(ok.file.declarations[0].fields[1].type_spelling == "int");
-
-        assert(ok.file.declarations[0].fields[0].comments.size() == 1);
-        assert(ok.file.declarations[0].fields[0].comments[0].text ==
-               R"(json:"name")");
-        assert(ok.file.declarations[0].fields[1].comments.size() == 1);
-        assert(ok.file.declarations[0].fields[1].comments[0].text ==
-               R"(json:"age")");
-
-        auto missing =
-            cjm::parser::parse_source_file("examples/basic/missing.hpp");
-        assert(!missing.success);
-        assert(missing.error.location.file == "examples/basic/missing.hpp");
-    }
     return 0;
 }
