ca239a330813a6f3fa6e469ced52234ab9d3d357
lmingzhi618 <lmingzhi618@gmail.com>
2026-08-06 23:14:36 -0400

    test(simdjson): verify narrow integer overflow error

--- tests/backends/simdjson/integer_generated_compile_tests.cpp
+++ tests/backends/simdjson/integer_generated_compile_tests.cpp
@@ -23,5 +23,18 @@ int main() {
     assert(error.path.empty());
     assert(error.runtime_error == simdjson::SUCCESS);
 
+    cjm::simdjson::DecodeError overflow_error;
+    const auto overflow_result = cjm::simdjson::from_json<IntegerValues>(
+        R"({"count": 0, "limit": 0, "narrow": 128})", overflow_error);
+
+    assert(!overflow_result.has_value());
+    assert(overflow_error.code ==
+           cjm::simdjson::DecodeErrorCode::integer_overflow);
+    assert(overflow_error.path.size() == 1);
+    assert(overflow_error.path[0].kind ==
+           cjm::simdjson::DecodePathSegmentKind::field);
+    assert(overflow_error.path[0].field_name == "narrow");
+    assert(overflow_error.runtime_error == simdjson::SUCCESS);
+
     return 0;
 }
