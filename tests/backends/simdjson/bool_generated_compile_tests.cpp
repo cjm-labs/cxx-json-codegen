6211d614cd77c3ee4acfbc43fc73bba8de93855c
lmingzhi618 <lmingzhi618@gmail.com>
2026-08-06 23:07:13 -0400

    test(simdjson): verify bool type mismatch error

--- tests/backends/simdjson/bool_generated_compile_tests.cpp
+++ tests/backends/simdjson/bool_generated_compile_tests.cpp
@@ -18,5 +18,19 @@ int main() {
     assert(error.path.empty());
     assert(error.runtime_error == simdjson::SUCCESS);
 
+    cjm::simdjson::DecodeError missing_error;
+
+    const auto missing_result =
+        cjm::simdjson::from_json<BoolValues>("{}", missing_error);
+
+    assert(!missing_result.has_value());
+    assert(missing_error.code ==
+           cjm::simdjson::DecodeErrorCode::missing_required_field);
+    assert(missing_error.path.size() == 1);
+    assert(missing_error.path[0].kind ==
+           cjm::simdjson::DecodePathSegmentKind::field);
+    assert(missing_error.path[0].field_name == "enabled");
+    assert(missing_error.runtime_error == simdjson::SUCCESS);
+
     return 0;
 }
